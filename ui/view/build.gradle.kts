ca3b0de1f6dec062d50fd6ed8aa1e9e914145b13
Ihor Martyniuk <devmartyniuk@gmail.com>
2025-06-30 22:42:20 +0300

    create root auth fragment

--- ui/view/build.gradle.kts
+++ ui/view/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.activity)
+    implementation(libs.androidx.fragment)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     //di
---
+++ ui/view/src/main/java/com/dev/martyniuk/local/chat/ui/view/auth/fragment/root/ViewModelAuthRoot.kt
@@ -0,0 +1,8 @@
+package com.dev.martyniuk.local.chat.ui.view.auth.fragment.root
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ViewModelAuthRoot @Inject constructor() : ViewModel()
