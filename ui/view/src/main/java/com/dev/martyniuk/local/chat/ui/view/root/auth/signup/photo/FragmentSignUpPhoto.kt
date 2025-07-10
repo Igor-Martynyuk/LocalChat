904e9669dc262144f8344f660cf1d76d4b9e3e06
Ihor Martyniuk <devmartyniuk@gmail.com>
2025-07-10 18:17:41 +0300

    setup SignUpPhoto binding

--- ui/view/src/main/java/com/dev/martyniuk/local/chat/ui/view/root/auth/signup/photo/FragmentSignUpPhoto.kt
+++ ui/view/src/main/java/com/dev/martyniuk/local/chat/ui/view/root/auth/signup/photo/FragmentSignUpPhoto.kt
@@ -1,19 +1,27 @@
 package com.dev.martyniuk.local.chat.ui.view.root.auth.signup.photo
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.dev.martyniuk.local.chat.ui.view.R
+import com.dev.martyniuk.local.chat.ui.view.databinding.FragmentAuthSingnUpPhotoBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class FragmentSignUpPhoto : Fragment() {
     private val viewModel: ViewModelSignUpPhoto by viewModels()
+    private lateinit var binding: FragmentAuthSingnUpPhotoBinding
 
-    override fun onCreateView(inflater: LayoutInflater, group: ViewGroup?, state: Bundle?): View? =
-        inflater.inflate(R.layout.fragment_auth_singn_up_photo, group, false)
+    override fun onCreateView(inflater: LayoutInflater, group: ViewGroup?, state: Bundle?): View =
+        FragmentAuthSingnUpPhotoBinding
+            .inflate(inflater)
+            .also {
+                it.lifecycleOwner = this
+                it.contract = viewModel
 
+                binding = it
+            }
+            .root
 }
--- ui/view/src/main/java/com/dev/martyniuk/local/chat/ui/view/root/auth/signup/photo/ViewModelSignUpPhoto.kt
+++ ui/view/src/main/java/com/dev/martyniuk/local/chat/ui/view/root/auth/signup/photo/ViewModelSignUpPhoto.kt
@@ -5,4 +5,4 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class ViewModelSignUpPhoto @Inject constructor() : ViewModel()
+class ViewModelSignUpPhoto @Inject constructor() : ViewModel(), ContractSignUpPhoto
